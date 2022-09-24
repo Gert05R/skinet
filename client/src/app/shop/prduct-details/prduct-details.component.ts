5539f332ade4279c002acea812e5a0ab0d00829e
GERT05 <raeymaekers.gert1@gmail.com>
2022-09-24 14:57:07 +0200

    End of section 10

---
+++ client/src/app/shop/prduct-details/prduct-details.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
+
+@Component({
+  selector: 'app-prduct-details',
+  templateUrl: './prduct-details.component.html',
+  styleUrls: ['./prduct-details.component.scss']
+})
+export class PrductDetailsComponent implements OnInit {
+  product: IProduct;
+
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe(product => {
+      this.product = product;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+}
